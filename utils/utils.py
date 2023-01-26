b73f17d62c453e4c9957890fe168912356339b17
SanaAwan5 <47128808+SanaAwan5@users.noreply.github.com>
2023-01-26 12:01:29 -0600

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -18,3 +18,12 @@ def dict_html(dict_obj, current_time):
     output = f'<h4>Params for model: {current_time}:</h4><table>{out}</table>'
     return output
 
+def mask_grad_update_by_magnitude(grad_update, mask_constant):
+
+	# mask all but the updates with larger magnitude than <mask_constant> to zero
+	# print('Masking all gradient updates with magnitude smaller than ', mask_constant)
+	grad_update = copy.deepcopy(grad_update)
+	for i, update in enumerate(grad_update):
+		grad_update[i].data[update.data.abs() < mask_constant] = 0
+	return grad_update
+
